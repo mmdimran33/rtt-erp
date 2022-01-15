0a93cadb0885ca87b7127110b8568d1d08c45df5
fjee97 <you@example.com>
2022-01-15 17:49:30 +0530

    comitting new erp product service

---
+++ src/main/java/com/roottronics/erp/productservice/rtterpproductservice/RttErpProductServiceApplication.java
@@ -0,0 +1,13 @@
+package com.roottronics.erp.productservice.rtterpproductservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RttErpProductServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RttErpProductServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/roottronics/erp/productservice/rtterpproductservice/RttErpProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.roottronics.erp.productservice.rtterpproductservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RttErpProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
